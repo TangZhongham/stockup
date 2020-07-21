22920b7e50da411853317f9ac1700870be9cc20f
唐某某 <tangmoumou@tangmoumoudeMacBook-Pro.local>
2020-07-21 19:24:05 +0800

    formatter_init

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,6 +1,19 @@
 extern crate colored;
 use colored::*;
 
-pub fn beautify(line: &str) -> Vec<&usize> {
+pub fn beautify(line: &str) {
+    println!("{}", line.blue());
+    let v = vec!["a", "b", "c", "d"];
 
+    for i in v.iter() {
+        println!("A reference to {}", i);
+    };
+
+    let r = String::from("xyz").green().bold();
+    // r
+}
+
+#[test]
+fn test_beautify() {
+    let a = beautify("a");
 }
