fc7e06eb3b7b1b6f0f6f9ebf6e3014be4b99e2f0
唐某某 <tangmoumou@tangmoumoudeMacBook-Pro.local>
2020-07-23 11:47:54 +0800

    20200723_version0.8_add_cli_tool

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,34 @@ mod extractor;
 extern crate colored;
 
 use std::collections::HashMap;
+use reqwest::IntoUrl;
 use colored::*;
 use std::time::Duration;
 use std::thread::sleep;
 
+use clap::{Arg, App};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    // let v = vec![];
-    // let mut v: Vec<f32> = Vec::new();
+    let matches = App::new("Stock it up...")
+        .version("v0.5")
+        .author("Tang Zhongham <13122260573@163.com>")
+        .about("监控股票价格如: sz000802")
+        .arg(Arg::with_name("stock_list")
+            .short("s".parse().unwrap()).long("stocks")
+            .takes_value(true).help("股票列表: sz000802,sz000803"))
+        .arg(Arg::with_name("refresh_time")
+            .short("t".parse().unwrap()).long("time")
+            .takes_value(true).help("刷新时间, 默认5s"))
+        .get_matches();
+
+    let default_stock = "sz000801";
+    let source_url = "http://hq.sinajs.cn/list=".to_string();
+    let stock_list = matches.value_of("stock_list").unwrap_or(default_stock);
+    let url = source_url + stock_list;
+    // let stock_list: Vec<&str> = _stock_list.split(",").collect();
+
     let mut v = vec![1.0];
 
     let head = format!("{}  {}  {}  {}  {}  {}",
@@ -21,8 +40,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let five_seconds = Duration::new(5, 0);
     loop {
         let former_price = v.pop();
-        println!("{}", former_price.unwrap().to_string());
-        let price = run("http://hq.sinajs.cn/list=sz000802", former_price.unwrap_or(1.0)).await?;
+        let price = run(&url, former_price.unwrap_or(1.0)).await?;
         v.push(price);
         sleep(five_seconds);
     }
@@ -40,10 +58,27 @@ async fn run(url: &str, former_price: f32) -> Result<f32, Box<dyn std::error::Er
         false
     };
 
-    println!("{}", greater.to_string());
 
     let a = formatter::beautify(x, Some(greater));
     // println!("{}", a.unwrap());
 
     Ok(price)
+}
+
+#[tokio::test]
+async fn test_url() -> Result<(), Box<dyn std::error::Error>> {
+    let default_stock = "sz000802";
+    let matches = App::new("Stock on my way...")
+        .version("0.5")
+        .author("Tang Zhongham <13122260573@163.com")
+        .about("监控股票价格: sz000802")
+        .arg(Arg::with_name("stock_list")
+            .short("s".parse().unwrap()).long("stocks")
+            .takes_value(true).help("股票列表: sz000802,sz000803"))
+        .get_matches();
+    let stock_list = matches.value_of("stock_list").unwrap_or(default_stock);
+    let source_url = "http://hq.sinajs.cn/list=".to_string();
+    let url = source_url + stock_list;
+    println!("{}", url);
+    Ok(())
 }
